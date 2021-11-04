ccf7e1f2997515381c5239eca623ba80273bc9cf
AlexanderNormann <alex654v@stud.kea.dk>
2021-11-04 11:49:30 +0100

    3

---
+++ wishlistv2/src/main/java/com/example/wishlistv2/respositories/BrugerRespositoryStub.java
@@ -0,0 +1,24 @@
+package com.example.wishlistv2.respositories;
+
+
+import com.example.wishlistv2.domain.model.Bruger;
+import com.example.wishlistv2.domain.servives.LoginSampleException;
+
+public class BrugerRespositoryStub implements BrugerRepository {
+  @Override
+  public Bruger login(String email, String kodeord) throws LoginSampleException {
+    Bruger bruger = new Bruger("start.com", "test");
+    bruger.setId(1);
+    return bruger;
+  }
+
+  @Override
+  public Bruger opretBruger(Bruger bruger) throws LoginSampleException {
+    return null;
+  }
+
+  @Override
+  public void gem(Bruger bruger) {
+
+  }
+}
